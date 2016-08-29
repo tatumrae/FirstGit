d17029d323f1402fa8a34a459284abd3c01f716b
Tatum <Tatum@10.30.21.39>
2016-08-29 18:37:29 -0500

    First commit

---
+++ src/firstgit/FirstGit.java
@@ -0,0 +1,17 @@
+package firstgit;
+
+/**
+ *
+ * @author Tatum
+ */
+public class FirstGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world");
+    }
+    
+}
